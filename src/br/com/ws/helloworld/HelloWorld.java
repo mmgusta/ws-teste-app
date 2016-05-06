24573653a28047cfe162f96c18b625a325c82d97
gustavo martins <gustavo.marinelli@gmail.com>
2016-05-05 23:06:18 -0300

    exemplo de ws restful

---
+++ src/br/com/ws/helloworld/HelloWorld.java
@@ -0,0 +1,19 @@
+package br.com.ws.helloworld;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.Response;
+
+@Path("")
+public class HelloWorld {
+	
+	
+	@GET
+	@Path("/helloWorld")
+	@Produces("application/json")
+	public Response getHelloWorld() {
+		String value = "Hello world";
+		return Response.status(200).entity(value).build();
+	}
+}
